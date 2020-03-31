610e8c2050697d9b4b15a390421bd278eb9a94e5
Clay Negen <negenc@mail.gvsu.edu>
2020-03-31 16:03:20 -0400

    Images and Data definition

---
+++ application/src/data.js
@@ -0,0 +1,20 @@
+const data = [
+  {
+    fname: "Clay",
+    lname: "Negen",
+    business: "U-Pick Farms",
+    address: "111 Amazing Street",
+    city: "The Big Apple",
+    country: "USA",
+    hoursFrom: "8am",
+    hoursTo: "5pm",
+    website: "upickfarms.com",
+    description: "description",
+    products: ["apples", "pumpkins"],
+    itemData: {
+      title: "Upick Famrs",
+      hours: "8 to 5",
+      img: "./public/apples/1.jpg"
+    }
+  }
+];
