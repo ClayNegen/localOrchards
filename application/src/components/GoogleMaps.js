f34b8b5785387268eba14af5709074873c906041
Clay Negen <negenc@mail.gvsu.edu>
2020-04-13 19:22:44 -0400

    Working GeoCoder and map selection / re-render

--- application/src/components/GoogleMaps.js
+++ application/src/components/GoogleMaps.js
@@ -3,7 +3,7 @@ import React, { useEffect, useRef } from "react";
 // Variables
 const GOOGLE_MAP_API_KEY = "AIzaSyA8I9ttWAFKaW7E8tZ9GC2ktb21PRAGmWk";
 const myLocation = {
-  // CN Tower Landmark
+  //Grand Rapids
   lat: 42.9634,
   lng: -85.6681,
 };
@@ -14,6 +14,28 @@ const mapStyles = {
 };
 
 function GoogleMaps(props) {
+  const [lat, setLat] = React.useState("");
+  const [lng, setLng] = React.useState("");
+  let address = props.location.address;
+  address = address.replace(/\s+/g, "+");
+  let city = props.location.city;
+  city = city.replace(/\s+/g, "+");
+  let state = props.location.state;
+  let loc = address + ",+" + city + ",+" + state;
+  console.log("loc, ", loc);
+  let link = `https://maps.googleapis.com/maps/api/geocode/json?address=${loc}&key=${GOOGLE_MAP_API_KEY}`;
+
+  React.useEffect(() => {
+    fetch(link)
+      .then((res) => res.json())
+      .then((response) => {
+        setLat(response.results[0].geometry.location.lat);
+        setLng(response.results[0].geometry.location.lng);
+        console.log("Set: ", response.results[0].geometry.location);
+      })
+      .catch((error) => console.log("Error: ", error));
+  });
+
   // refs
   const googleMapRef = React.createRef();
   const googleMap = useRef(null);
@@ -24,19 +46,21 @@ function GoogleMaps(props) {
     new window.google.maps.Map(googleMapRef.current, {
       zoom: 14,
       center: {
-        lat: myLocation.lat,
-        lng: myLocation.lng,
+        lat: lat,
+        lng: lng,
       },
     });
 
+  console.log("Lat: ", lat, "lng: ", lng);
   const createMarker = () =>
     new window.google.maps.Marker({
-      position: { lat: myLocation.lat, lng: myLocation.lng },
+      position: { lat: lat, lng: lng },
       map: googleMap.current,
     });
 
   // useEffect Hook
   useEffect(() => {
+    //Maps
     const googleMapScript = document.createElement("script");
     googleMapScript.src = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAP_API_KEY}&libraries=places`;
     window.document.body.appendChild(googleMapScript);
--- application/src/components/Maps.js
+++ application/src/components/Maps.js
@@ -21,12 +21,14 @@ const sections = [
 ];
 
 export default function Maps() {
+  const [location, setLocation] = React.useState(null);
   const [places, setPlaces] = React.useState([]);
 
   React.useEffect(() => {
     const fetchData = async () => {
       db.collection("users").onSnapshot(function (data) {
         setPlaces(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+        setLocation(data.docs.map((doc) => ({ ...doc.data() })));
       });
     };
 
@@ -35,6 +37,22 @@ export default function Maps() {
 
   console.log("Places: ", places);
 
+  const clicked = (item) => {
+    console.log("clicked: ", item);
+    let l = [
+      {
+        address: item.address,
+        city: item.city,
+        state: item.state,
+      },
+    ];
+    setLocation(l);
+    console.log("LocoLoco: ", l);
+  };
+
+  const GoogooMap = location ? <GoogleMaps location={location[0]} /> : null;
+  console.log("Location: ", location);
+
   return (
     <div className="Maps">
       <React.Fragment>
@@ -42,13 +60,15 @@ export default function Maps() {
           <Header title="Local Orchards" sections={sections} />
           <main>
             <Card>
-              <GoogleMaps />
+              {GoogooMap}
               {places.map((item, index) => (
                 <Place
                   key={index}
                   title={item.business_title}
                   url={item.website}
                   des={item.description}
+                  value={item.address}
+                  callback={() => clicked(item)}
                 />
               ))}
             </Card>
@@ -65,14 +85,14 @@ export default function Maps() {
 
 function Place(props) {
   return (
-    <CardActionArea>
+    <CardActionArea onClick={props.callback}>
       <CardContent>
         <Typography gutterBottom variant="h5" component="h2"></Typography>
         {props.title}
         <Typography variant="body2" color="textSecondary" component="p">
           {props.des}
         </Typography>
-        <Link to={props.url}>{props.url}</Link>
+        <a href={props.url}>{props.url}</a>
       </CardContent>
     </CardActionArea>
   );
